01e0ab40bf5db92f33b6e47b9705373ca55caeec
PeterThi <pete9817@gmail.com>
2023-12-04 10:25:44 +0100

    SharedModels init

---
+++ SharedModels/CustomerDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace SharedModels
+{
+    public class CustomerDTO
+    {
+        public int CustomerId;
+
+        public string Name;
+
+        public string PhoneNr;
+
+        public string Email;
+
+        public int Age;
+
+        //notice that we don't transfer balance... maybe
+        //public int Balance;
+    }
+}
