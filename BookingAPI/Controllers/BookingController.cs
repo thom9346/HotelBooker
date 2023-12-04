d590a2966a1e9fc27de351bb0ccc345979bbeec4
thom9346 <holmegaard1996@gmail.com>
2023-12-04 10:52:26 +0100

    deleted default whetherforecasts

---
+++ BookingAPI/Controllers/BookingController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookingController : ControllerBase
+    {
+    
+    }
+}
