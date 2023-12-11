e95e16d979b64816c7b9b1ccefaa42bbce2b30a7
narerv01 <narerv01@easv365.dk>
2023-12-11 12:46:01 +0100

    HotelRoom Og Booking object tilføjet

---
+++ BookingApi/Models/Booking.cs
@@ -0,0 +1,10 @@
+﻿namespace BookingApi.Models
+{
+    public class Booking
+    { 
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int HotelRoomId { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
